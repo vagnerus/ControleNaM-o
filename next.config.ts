65f1efd8a8208abc1247fdfea200d2d2619511b8
Vagner Oliveira <vagneroliveira.us@gmail.com>
2025-12-30 22:07:45 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -2,24 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    // This is required to fix a bug with Turbopack not resolving the project root correctly.
-    turbopack: {
-      resolveExtensions: [
-        '.mdx',
-        '.tsx',
-        '.ts',
-        '.jsx',
-        '.js',
-        '.mjs',
-        '.json',
-      ],
-      // The `next` and `react` packages are not in `node_modules` of the app directory.
-      // This is a workaround to make sure that Turbopack can find them.
-      // https://github.com/vercel/next.js/issues/66870
-      root: process.cwd(),
-    },
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
