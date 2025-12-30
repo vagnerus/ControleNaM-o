20ba91c4a2bf4fee736139faa87d6e3ce3ca6d38
Vagner Oliveira <vagneroliveira.us@gmail.com>
2025-12-30 20:45:00 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7552698109-f3e43",
+  "appId": "1:393001354142:web:1c533107063b9940980c8f",
+  "apiKey": "AIzaSyDtT9deN-CUnjD44TSibjB7yOH_6Qvlz2I",
+  "authDomain": "studio-7552698109-f3e43.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "393001354142"
+};
