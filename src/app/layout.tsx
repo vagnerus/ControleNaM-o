e1a8ce9b340ea6dedcdef5f125a768b40ebecdac
Vagner Oliveira <vagneroliveira.us@gmail.com>
2025-12-30 20:49:05 +0000

    Try fixing this error: `Runtime Error: Could not find the module "[proje

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
-import { FirebaseClientProvider } from '@/firebase/client-provider';
+import { FirebaseClientProvider } from '@/firebase';
 import { AuthGate } from '@/components/auth/AuthGate';
 
 export const metadata: Metadata = {
